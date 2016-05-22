00356d582d481694d2b39d08bbe638af6f872ded
François Voron <fvoron@gmail.com>
2016-05-22 11:05:59 +0200

    Write a very simple unit test

--- twitterautoreplybot/main/tests.py
+++ twitterautoreplybot/main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from main.models import Campaign
 
-# Create your tests here.
+# Campaign model tests
+class CampaignTestCase(TestCase):
+    def setUp(self):
+        Campaign.objects.create(
+            name = "Test campaign",
+            answerSentence = "Username : %twitterUsername% / Magic word : %magicWord% / Tweet id : %tweetId%"
+        )
+            
+    def test_generateAnswerSentence(self):
+        campaign_test = Campaign.objects.get(name = "Test campaign")
+        self.assertEqual(
+            campaign_test.generateAnswerSentence("TwitterUser", "1234", "MagicWord"),
+            ("Username : TwitterUser / Magic word : MagicWord / Tweet id : 1234", [])
+        )
